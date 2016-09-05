ae235894393beee28bd353ec988336714ed010fb
XZCao <1970158871@qq.com>
2016-09-05 10:55:43 +0800

    创建了ThirdPartyFont使用第三方字体库

---
+++ ThirdPartyFont/ThirdPartyFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdPartyFont
+//
+//  Created by XZCao on 16/9/5.
+//  Copyright © 2016年 曹欣宗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
